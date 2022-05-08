5c4b630d6d3f449b464b547df6f627292e7d8a1d
valterchess <valterrekabano7@gmail.com>
2022-05-08 20:30:30 -0300

    testado consumo de imagens

--- main.js
+++ main.js
@@ -5,10 +5,24 @@ function getApi(){
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200){
             const response = JSON.parse( xhr.responseText);
+             const results = response.results;
             console.log(response);
+            getChars(results);
         }
     }
     xhr.open('GET', uri, true);
     xhr.send();
+}
+
+function getChars(results){
+    results.forEach(ele => {
+        const img = document.createElement('img');
+        const main =  document.querySelector('#main');
+
+        img.src = ele.image;
+        img.classList.add('img-fluid');
+
+        main.append(img);
+    });
 }
 getApi();
